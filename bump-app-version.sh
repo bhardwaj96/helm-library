2e052d8be8667742798d2393f67073410ba0c0c0
bhardwaj96 <71921617+bhardwaj96@users.noreply.github.com>
2022-02-02 17:23:41 +0530

    Update bump-app-version.sh

--- bump-app-version.sh
+++ bump-app-version.sh
@@ -44,9 +44,9 @@ sed -i "s#^version:.*#version: ${TAG/v/}#g" "${CHART_YAML}"
 
 ## useful for debugging purposes
 git status
-password=Dhakkan@96
+
 ## Add new remote with credentials baked in url
-git remote add publisher https://bhardwaj96:${Dhakkan96}@github.com/bhardwaj96/helm-library.git
+git remote add publisher https://bhardwaj96:{Dhakkan96}@github.com/bhardwaj96/helm-library.git
 # git remote add publisher "https://github.com/bhardwaj96/helm-library.git"
 
 CHANGE_COUNT=$(git status --porcelain | wc -l)
